0ace24a9be873308994a82458a7c0bba823f5dd3
Pradyumna Garg <pradyumna.garg@contentstack.com>
2025-12-23 10:21:07 +0530

    fix: snyk issues and vite update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitest.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
